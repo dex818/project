cf2c04bfaf03cc1f36e6db068a25d82d146bb651
dex818 <mohand050500@gmail.com>
2023-09-27 10:38:07 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+include './inc/db.php';
+include './inc/form.php';
+include './inc/select.php';
+include './inc/db_close.php';
+?>
+
+
+<?php include_once './parts/header.php'; ?>
+
+
+     
+
+     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="col-md-5 p-lg-5 mx-auto">
+        <img src="images/kia.jpg" alt="">
+      <h1 class="display-4 fw-normal">اربح مع مهند فهد البلوي</h1>
+      <p class="lead fw-normal">لم يبقى الا القليل على التسجيل اغتنم فرصتك</p>
+      <h3 id="countdown"></h3>
+      <p class="lead fw-normal"> للسحب على سيارة كيا سيراتو </p>
+      
+    </div>
+    
+    <div class="container">
+    <ul class="list-group list-group-flush">
+        <h3>للدخول في السحب اتبع ما يلي:</h3>
+
+<li class="list-group-item list-group-item-primary">تابع البث المباشر على صفحتي على فيسبوك بالتاريخ المذكور أعلاه</li>
+<li class="list-group-item list-group-item-warning">يبث مباشر لمدة ساعة عبارة عن اسئلة واجوبة حرة للجميع</li>
+<li class="list-group-item list-group-item-primary">ثم فتح صفحة التسجيل هنا حيث ستقوم بتسجيل اسمك وايميلك</li>
+<li class="list-group-item list-group-item-warning">حيث سيتم اختيار اسم واحد من قاعدة البيانات بشكل عشوائي</li>
+<li class="list-group-item list-group-item-primary">الرابح سيحصل على سيارة كيا موديل 2023</li>
+</ul>
+    </div>
+
+  </div>
+ 
+ 
+
+<div class="container">
+ <div class="position-relative  text-right ">
+     <div class="col-md-5 p-lg-5 mx-auto my-5">
+  <form  action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+           <h3>الرجاء ادخل معلوماتك</h3> 
+  <div class="mb-3">
+    <label for="firstName" class="form-label">الاسم الاول</label>
+    <input type="text " name="firstName" id="firstName" class="form-control"  value="<?php echo $firstName ?>">
+    <div class="form-text error"><?php echo $errors['firstNameError']?></div>
+  </div>
+
+
+  <div class="mb-3">
+    <label for="lastName" class="form-label">الاسم الاخير</label>
+    <input type="text" name="lastName" id="lastName" class="form-control"  value="<?php echo $lastName ?>">
+    <div class="form-text error"><?php echo $errors['lastNameError']?></div>
+  </div>
+
+
+  <div class="mb-3">
+    <label for="email" class="form-label">البريد الاكتروني</label>
+    <input type="text" name="email" id="email" class="form-control"  value="<?php echo $email ?>">
+    <div class="form-text error"><?php echo $errors['emailError']?></div>
+  </div>
+  
+  
+  <button type="submit" name="submit" class="btn btn-primary">أرسال المعلومات</button>
+</form>
+</div>
+</div>
+
+
+
+<div class="loader-con">
+     <div id="loader">
+	<canvas id="circularLoader" width="200" height="200"></canvas>
+    </div>
+</div>
+
+
+
+<!-- Button trigger modal -->
+<div class="d-grid gap-2 col-6 mx-auto my-5">
+<button type="button" id="winner" class="btn btn-primary" >
+  اختيار الرابح
+</button>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+      
+        <h5 class="modal-title" id="modalLabel">الرابح في المسابقة</h5>
+        
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <?php foreach($users as $user): ?>
+        <h1 class="display-3 text-center modal-title" id="modalLabel"><?php echo htmlspecialchars($user['firstName']). ' ' .htmlspecialchars($user['lastName']) ?> </h1>
+        <?php endforeach; ?>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+<?php include_once './parts/footer.php';   ?>
---
+++ parts/footer.php
@@ -0,0 +1,8 @@
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ></script>
+     <script src="./JS/loader.js"></script>
+     <script src="./js/script.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"></script>
+</body>
+
+</html>
